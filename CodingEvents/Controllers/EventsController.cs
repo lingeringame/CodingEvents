fe69fe228660120be97cd04fbec8be08ef6a7dd3
sidereal <holasoyserg@gmail.com>
2022-10-27 16:27:05 -0500

    Add project files.

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.events = Events;
+
+            return View();
+        }
+        // GET: /<controller>/add
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("/events/add")]
+        public IActionResult NewEvent(string name)
+        {
+            Events.Add(name);
+
+            return Redirect("/events");
+        }
+    }
+}
